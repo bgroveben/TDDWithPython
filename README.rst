29bdbc6bf25025ef9e3f087c97586d3cdd9bb657
bgroveben <bgroveben@gmail.com>
2016-07-27 11:04:04 -0700

    Import unittest from Python Standard Library and set up a NewVisitorTest class

--- README.rst
+++ README.rst
@@ -7,4 +7,4 @@ Test-Driven Development with Python
 
 The code for this book can be found at http://www.obeythetestinggoat.com/pages/book.html#toc
 
-I am currently at http://www.obeythetestinggoat.com/book/chapter_02.html#_the_python_standard_library_s_unittest_module
+I am currently at http://www.obeythetestinggoat.com/book/chapter_02.html#_implicit_waits
--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,29 +1,40 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
+import unittest
 
-# Ben has heard about a tutorial that uses an online to-do app (Yay! Time to build stuff!)
-# Ben goes to check out the homepage of the to-do list app.
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# He notices that the page title and header mention a to-do list
-assert 'To-Do' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# Ben sees that he is invited to enter an item on the list right away, and he gets excited.
+    def tearDown(self):
+        self.browser.quit()
 
-# He types "Start building to-do app" into a text box.
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Ben has heard about a tutorial that uses an online to-do app (Yay! Time to build stuff!)
+        # Ben goes to check out the homepage of the to-do list app.
+        self.browser.get('http://localhost:8000')
 
-# When Ben hits enter, the page updates, and now the page lists
-# "1: Start building to-do app" as an item in the to-do list.
+        # He notices that the page title and header mention a to-do list
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-# There is still a text box inviting, daring, coercing Ben to add another item.
-# He enters "Write a bunch of tests for the to-do list app".
+        # Ben sees that he is invited to enter an item on the list right away, and he gets excited.
 
-# The page updates again, and now shows both items on the list.
+        # He types "Start building to-do app" into a text box.
 
-# Ben wonders if the site will remember his list.
-# Then he sees that the site has generated a unique URL for him -- there is some text that explains it.
+        # When Ben hits enter, the page updates, and now the page lists
+        # "1: Start building to-do app" as an item in the to-do list.
 
-# Ben visits that URL - his to-do-list is still there.
+        # There is still a text box inviting, daring, coercing Ben to add another item.
+        # He enters "Write a bunch of tests for the to-do list app".
 
-# Satisfied, Ben can finally go to sleep for the night.
-browser.quit()
+        # The page updates again, and now shows both items on the list.
+
+        # Ben wonders if the site will remember his list.
+        # Then he sees that the site has generated a unique URL for him -- there is some text that explains it.
+
+        # Ben visits that URL - his to-do-list is still there.
+
+        # Satisfied, Ben can finally go to sleep for the night.
+if __name__ == '__main__':
+    unittest.main()
