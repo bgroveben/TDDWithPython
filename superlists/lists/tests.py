1c7c883fb3917b88b17a52bcd601807b4fab1e70
bgroveben <bgroveben@gmail.com>
2016-07-28 06:57:46 -0700

    Write unit test to ensure that the home page returns the correct HTML

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,8 @@
-from django.test import TestCase
-
 from django.core.urlresolvers import resolve
-from lists.views import home_page  # Which we haven't built yet -- tests first
+from django.test import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page
 
 class HomePageTest(TestCase):
 
@@ -9,3 +10,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolvers_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    # Can we make this view function return some HTML which will get the functional test to pass?
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
