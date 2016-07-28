ac8600125b24a71cff548c9c781ec877eb0368d6
bgroveben <bgroveben@gmail.com>
2016-07-28 13:25:05 -0700

    Write a unit test to ensure that the home_page view can handle a POST request

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -18,3 +18,14 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        # Notes about the line spacing and how the lines are grouped:
+        # Setup -- set up the test
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        # Exercise -- the line in the middle actually calls the function under test
+        response = home_page(request)
+        # Assert -- assertions at the end
+        self.assertIn('A new list item', response.content.decode())
