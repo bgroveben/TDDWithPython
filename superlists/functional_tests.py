345433dd9c3b074a795bd21de21e002e0e0fa8fa
bgroveben <bgroveben@gmail.com>
2016-07-28 08:26:48 -0700

    Change my user story to match author's in functional test file (so I don't get confused)

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -5,37 +5,41 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3) # seconds
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Ben has heard about a tutorial that uses an online to-do app (Yay! Time to build stuff!)
-        # Ben goes to check out the homepage of the to-do list app.
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
         self.browser.get('http://localhost:8000')
 
-        # He notices that the page title and header mention a to-do list
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
 
-        # Ben sees that he is invited to enter an item on the list right away, and he gets excited.
+        # She is invited to enter a to-do item straight away
 
-        # He types "Start building to-do app" into a text box.
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
 
-        # When Ben hits enter, the page updates, and now the page lists
-        # "1: Start building to-do app" as an item in the to-do list.
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
 
-        # There is still a text box inviting, daring, coercing Ben to add another item.
-        # He enters "Write a bunch of tests for the to-do list app".
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
-        # The page updates again, and now shows both items on the list.
+        # The page updates again, and now shows both items on her list
 
-        # Ben wonders if the site will remember his list.
-        # Then he sees that the site has generated a unique URL for him -- there is some text that explains it.
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
 
-        # Ben visits that URL - his to-do-list is still there.
+        # She visits that URL - her to-do list is still there.
 
-        # Satisfied, Ben can finally go to sleep for the night.
+        # Satisfied, she goes back to sleep
+
+
 if __name__ == '__main__':
     unittest.main()
