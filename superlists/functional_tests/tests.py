6e79e360570536dd0ed82c73ebf036eb4e43da25
bgroveben <bgroveben@gmail.com>
2016-08-04 15:08:43 -0700

    Improve the site's styling

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,3 +1,4 @@
+# from django.test import LiveServerTestCase -- https://docs.djangoproject.com/en/1.8/howto/static-files/#testing
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
