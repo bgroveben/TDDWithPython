b1bb30ea5ca2809fdb2c37dac97272addf90f896
bgroveben <bgroveben@gmail.com>
2016-08-24 18:07:34 -0700

    Refactor -- find and replace id_new_item and item_text -- get unit tests passing

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,7 +2,9 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 import sys
 
+
 class FunctionalTest(StaticLiveServerTestCase):
+
 
     @classmethod
     def setUpClass(cls):
@@ -13,14 +15,17 @@ class FunctionalTest(StaticLiveServerTestCase):
         super().setUpClass()
         cls.server_url = cls.live_server_url
 
+
     @classmethod
     def tearDownClass(cls):
         if cls.server_url == cls.live_server_url:
             super().tearDownClass()
 
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
+
 
     def tearDown(self):
         self.browser.quit()
@@ -30,3 +35,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
