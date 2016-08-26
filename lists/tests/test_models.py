0b1604eb667b7a5c2985541c46573e7324e2fb2b
bgroveben <bgroveben@gmail.com>
2016-08-26 00:01:25 -0700

    Write tests to deal with queryset ordering and string representations

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -60,3 +60,19 @@ class ListAndItemModelsTest(TestCase):
         Item.objects.create(list=list1, text='duplicate')
         item = Item(list=list2, text='duplicate')
         item.full_clean()  #! This should not raise an error
+
+
+    def test_list_ordering(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='item1')
+        item2 = Item.objects.create(list=list1, text='item2')
+        item3 = Item.objects.create(list=list1, text='item3')
+        self.assertEqual(
+            Item.objects.all(),
+            [item1, item2, item3]
+        )
+
+
+    def test_string_representation(self):
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
