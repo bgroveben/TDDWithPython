8817db89c76af5657382196525ed94cf39fa76c2
bgroveben <bgroveben@gmail.com>
2016-08-31 16:22:10 -0700

    Tell Django to send all exceptions to the terminal by default

--- superlists/settings.py
+++ superlists/settings.py
@@ -109,3 +109,22 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+
+# Tell Django to send all exceptions to the terminal by default
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
