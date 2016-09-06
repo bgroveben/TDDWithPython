c0d117161b32c355fb8ebc65eec295fa8501be58
bgroveben <bgroveben@gmail.com>
2016-09-06 01:38:45 -0700

    Add authentication backend to settings file

--- superlists/settings.py
+++ superlists/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = (
 )
 
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PasswordlessAuthenticationBackend',
+)
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
