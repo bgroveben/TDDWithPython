b67f94ff9f5223c3748c52867fecbe52840759a6
bgroveben <bgroveben@gmail.com>
2016-09-07 04:37:34 -0700

    Django's authentication framework expects the user model to have a last_login field

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,9 @@
 import uuid
+from django.contrib import auth
 from django.db import models
+
+# Django's authentication framework expects the user model to have a last_login field
+auth.signals.user_logged_in.disconnect(auth.models.update_last_login)
 
 
 class User(models.Model):
